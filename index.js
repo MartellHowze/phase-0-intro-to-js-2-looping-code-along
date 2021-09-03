c902b4a8242f42dac5387c37ef0c1e79161b3efe
Martell Howze <martellhowze042988@gmail.com>
2021-09-02 21:50:00 -0500

    commit tree 0d1f7cac52420502855ec7373dab4452a7252799

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+const friend = ['Lisa', 'Kaitlin', 'Jan'];
+//created an array named "friend"
+
+
+function writeCards(friend, event){
+//function takes in two arguments, the "friend" array and an event
+    let newFriendArr = []
+//per readme, created a new array to hold "friend" list with message added
+
+    for (let i = 0; i < friend.length; i++){
+//standard for loop syntax
+   
+    newFriendArr.push(`Thank you, ${friend[i]}, for the wonderful ${event} gift!`)
+//used .push method to combine the message and "friend" array in order to create the "newFriendArr"
+}
+
+return newFriendArr;
+//make sure to return outside of the loop function 
+}
+
+
+function countDown(int){
+    // let int = 10
+    
+    
+    while (int >= 0) {
+        console.log(int)
+         int--
+    }
+
+}
